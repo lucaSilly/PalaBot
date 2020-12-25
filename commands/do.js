a1a6ac77c4b6e0ac2076117841daa98659e11fa7
lucacahuette <lucacahuette@gmail.com>
2020-12-26 00:20:47 +0100

    commande handler

---
+++ commands/do.js
@@ -0,0 +1,16 @@
+const { execute } = require("./ping")
+
+const Discord = require('discord.js');
+
+module.exports = {
+    name : "do",
+    description : "crée une carte dans #build-a-faire",
+    execute(message, args){
+        const toDo = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(args[1])
+            
+
+    },
+
+};
--- index.js
+++ index.js
@@ -38,27 +38,6 @@ client.on('message', message => {
 });
 
 
-
-
-
-/*client.on("message",function(message){
-    if (message.author.bot) return;
-    if(!message.content.startsWith(prefix)) return;
-
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
-
-    if (command == "ping"){
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`pong! ${timeTaken}ms`);
-    }
-   
-    
-
-
-});*/
-
 client.on('error', console.error);
 
 client.login(process.env.BOT_TOKEN);
