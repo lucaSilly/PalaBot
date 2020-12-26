2d95838ca14a71210c562387a770726ae4687390
lucacahuette <lucacahuette@gmail.com>
2020-12-26 18:18:27 +0100

    do

--- commands/do.js
+++ commands/do.js
@@ -6,13 +6,14 @@ module.exports = {
     name : "do",
     description : "crée une carte dans #build-a-faire",
     execute(message, args){
+        args = args.filter(arg => arg != "");
         console.log(args);
         message.channel.send(new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle(args[0])
         .setDescription(args[1]+ " ")
-        //.addField("Deadline : ", args[2])
-        .addField("Deadline : ", (args[2].startsWith(" ")) ? args.splice(3) : args[2])
+        .addField("Deadline : ", args[2])
+        //.addField("Deadline : ", (args[2].startsWith(" ")) ? args.splice(3) : args[2])
         .addField("Builders :", args[3])
         
         );
