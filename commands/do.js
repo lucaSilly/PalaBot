2d08b679e8f0d9231b5df168c768450a64ebdef9
lucacahuette <lucacahuette@gmail.com>
2020-12-26 22:06:00 +0100

    test

--- commands/do.js
+++ commands/do.js
@@ -8,7 +8,7 @@ module.exports = {
     description : "crée une carte dans #build-a-faire",
     execute(message, args){
         args = args.filter(arg => arg != "");
-        message.client.channels.cache.get("746484079138046052").send(new Discord.MessageEmbed()
+        message.client.channels.cache.get("791360455431290900").send(new Discord.MessageEmbed()
         .setColor('#D94A2E')
         .setTitle(message.channel.name)
         .setDescription(args[1]+ " ")
@@ -21,8 +21,15 @@ module.exports = {
             await msg.react('🕑')
             await msg.react('➕')
         });
+        client.on('messageReactionAdd', (messageReaction, user)=>{
+            if(messageReaction.emoji.name=='✅'){
+                message.send("ça marche ouesh");
+            }
+
+        });
 
     },
    
+    //ajouter commanditaire, commande pour y accèder (quand validation de fin de map?),
 
 };
