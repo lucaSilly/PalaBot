aea451b0aba8fba8efdc6a90a1f0a29fb2d74c61
lucacahuette <lucacahuette@gmail.com>
2020-12-26 22:39:46 +0100

    react

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on('message', message => {
 });
 
 client.on('messageReactionAdd', (messageReaction, user)=>{
-    if(messageReaction.user != client.user.id) return;
+    if(user.id !== client.user.id) return;
     if(messageReaction.emoji.name=='✅'){
         client.channels.cache.get("791360455431290900").send('ça marche ouesh');
         
