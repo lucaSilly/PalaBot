6eca4901d9d2e14a747b4bd80f607ff5f6f08157
lucacahuette <lucacahuette@gmail.com>
2020-12-27 22:25:47 +0100

    test

--- index.js
+++ index.js
@@ -39,33 +39,7 @@ client.on('message', message => {
 
 });
 
-client.on('raw', async event => {
-	// `event.t` is the raw event name
-	if (!events.hasOwnProperty(event.t)) return;
-
-	const { d: data } = event;
-	const user = client.users.get(data.user_id);
-	const channel = client.channels.get(data.channel_id) || await user.createDM();
-
-	// if the message is already in the cache, don't re-emit the event
-	if (channel.messages.has(data.message_id)) return;
-
-	// if you're on the master/v12 branch, use `channel.messages.fetch()`
-	const message = await channel.fetchMessage(data.message_id);
-
-	// custom emojis reactions are keyed in a `name:ID` format, while unicode emojis are keyed by names
-	// if you're on the master/v12 branch, custom emojis reactions are keyed by their ID
-	const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
-	const reaction = message.reactions.get(emojiKey);
-
-	client.emit(events[event.t], reaction, user);
-});
-
-client.on('messageReactionAdd', (reaction, user) => {
-	console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
-});
-
-client.on('messageReactionAdd', (messageReaction, user)=>{
+/*client.on('messageReactionAdd', (messageReaction, user)=>{
     if(user.id == client.user.id) return;
     if(!messageReaction.message.author.bot) return;
     if(messageReaction.message.channel != '791360455431290900') return; //A MODIFIER
@@ -98,11 +72,12 @@ client.on('messageReactionAdd', (messageReaction, user)=>{
 
     }
 
-});
+});*/
 
-/*client.on('raw', e => {
+client.on('raw', e => {
     if(e.t === 'MESSAGE_REACTION_ADD'){
         if(user.id !==client.id && e.){
+            
 
         }
 
@@ -110,7 +85,7 @@ client.on('messageReactionAdd', (messageReaction, user)=>{
     console.log(e);
 
 
-});*/
+});
 
 
 
